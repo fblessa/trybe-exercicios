363aa151090acd1481f3bb9311db8baf05a6991f
Fabio Lessa <contatofabiolessa@gmail.com>
2023-07-06 12:25:25 -0300

    Descobre o maior valor do array - quesito 5

--- fundamentos/secao-03-intro-javascript/dia-02-array-estrutura-de-repeticao/exercicio-01.js
+++ fundamentos/secao-03-intro-javascript/dia-02-array-estrutura-de-repeticao/exercicio-01.js
@@ -1,4 +1,16 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let highValue = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > highValue) {
+    highValue = numbers[i];
+  }
+}
+
+console.log(highValue);
+
+
 let sum = 0;
 
 for (let i = 0; i < numbers.length; i++){
