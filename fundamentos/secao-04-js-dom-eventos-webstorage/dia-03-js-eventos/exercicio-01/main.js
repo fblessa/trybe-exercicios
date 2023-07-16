9a0220840549eb64df0bccb669d9326939623989
Fabio Lessa <contatofabiolessa@gmail.com>
2023-07-16 19:01:52 -0300

    Finalizando requisitos 4, 5

--- fundamentos/secao-04-js-dom-eventos-webstorage/dia-03-js-eventos/exercicio-01/main.js
+++ fundamentos/secao-04-js-dom-eventos-webstorage/dia-03-js-eventos/exercicio-01/main.js
@@ -5,11 +5,11 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
 
-// 1. Copie esse arquivo e edite apenas ele.
-// 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// // 1. Copie esse arquivo e edite apenas ele.
+// // 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 let resposta = 'Porque ela possui uma classe diferente, que está recebendo uma propriedade CSS específica!';
-// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
-// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
+// // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
+// // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
 const changeClass = (event) => {
   const techElement = document.querySelector('.tech');
   techElement.classList.remove('tech');
@@ -19,8 +19,8 @@ const changeClass = (event) => {
 firstLi.addEventListener('click' changeClass);
 secondLi.addEventListener('click' changeClass);
 thirdLi.addEventListener('click' changeClass);
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech'.
+// // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// // com a classe 'tech'.
 input.addEventListener('input', (event) => {
   const techElement = document.querySelector('.tech');
   techElement.innerText = event.target.value;
@@ -28,10 +28,17 @@ input.addEventListener('input', (event) => {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
-
+myWebpage.addEventListener('dblclick', () => {
+  window.open('https://blog.betrybe.com/');
+});
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
-
+myWebpage.addEventListener('mouseover', (event) => {
+  event.target.style.color = 'tomato';
+});
+myWebpage.addEventListener('mouseout', (event) => {
+  event.target.style.color = 'unset';
+});
 // Segue abaixo um exemplo de uso do event.target.
 
 const resetText = (event) => {
