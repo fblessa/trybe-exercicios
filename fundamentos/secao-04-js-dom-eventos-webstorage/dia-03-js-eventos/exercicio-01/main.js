486f559008241d804cbe617587cc983b860059c2
Fabio Lessa <contatofabiolessa@gmail.com>
2023-07-16 18:35:08 -0300

    realizando até o exercicio 3

--- fundamentos/secao-04-js-dom-eventos-webstorage/dia-03-js-eventos/exercicio-01/main.js
+++ fundamentos/secao-04-js-dom-eventos-webstorage/dia-03-js-eventos/exercicio-01/main.js
@@ -7,13 +7,24 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 1. Copie esse arquivo e edite apenas ele.
 // 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-
+let resposta = 'Porque ela possui uma classe diferente, que está recebendo uma propriedade CSS específica!';
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
-
+const changeClass = (event) => {
+  const techElement = document.querySelector('.tech');
+  techElement.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+firstLi.addEventListener('click' changeClass);
+secondLi.addEventListener('click' changeClass);
+thirdLi.addEventListener('click' changeClass);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech'.
-
+input.addEventListener('input', (event) => {
+  const techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
+});
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
