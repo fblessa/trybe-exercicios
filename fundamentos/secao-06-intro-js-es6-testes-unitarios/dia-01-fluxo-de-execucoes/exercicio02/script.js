b5f6ba49815db987ac2079a3706e393a83f42a6a
Fabio Lessa <contatofabiolessa@gmail.com>
2023-08-14 20:21:37 -0300

    Arrumando escopo do Try que estava errado e gerando erro na mensagem de Error

--- fundamentos/secao-06-intro-js-es6-testes-unitarios/dia-01-fluxo-de-execucoes/exercicio02/script.js
+++ fundamentos/secao-06-intro-js-es6-testes-unitarios/dia-01-fluxo-de-execucoes/exercicio02/script.js
@@ -148,16 +148,17 @@ const findPeopleByState = (state) => {
 // console.log((findPeopleByState('BA')));
 
 const studentRegister = (name, age) => {
-  if (!name || !age) {
-    throw new Error('Todas as informações são necessárias');
-  }
-  if (age < 18) {
-    throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
-  }
   try {
-    return `${name}, seja bem-vindo(a) à AuTrybe!`
-  } catch (error) {
+    if (!name || !age) {
+      throw new Error('Todas as informações são necessárias');
+    }
+    if (age < 18) {
+      throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
+    }
+    return `${name}, seja bem-vindo(a) à AuTrybe!`;
+  }
+  catch (error) {
     return error.message;
   }
 }
-// console.log(studentRegister('Fábio', 29));
+console.log(studentRegister('Fábio', 12));
