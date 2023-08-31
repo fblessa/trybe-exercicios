aa025e5ae5ad85bd5b0c55393d8b3a82b13560d8
Fabio Lessa <contatofabiolessa@gmail.com>
2023-08-31 13:42:40 -0300

    Realizando Exercicio

--- front-end/secao-01-Front-e-Js-Assincrono/dia-01-ambiente/validador-de-campos/src/main.js
+++ front-end/secao-01-Front-e-Js-Assincrono/dia-01-ambiente/validador-de-campos/src/main.js
@@ -1,2 +1,56 @@
 import validator from 'validator';
 import './style.css';
+
+const input = document.getElementById('input-date');
+const select = document.getElementById('selection');
+const validateBtn = document.getElementById('validation');
+const paragraph = document.getElementById('paragraph');
+
+const isEmail = () => {
+  const result = validator.isEmail(input.value);
+  paragraph.innerHTML = `A validação retornou ${result}`;
+};
+
+const isTaxId = () => {
+  const result = validator.isTaxID(input.value, 'pt-BR');
+  paragraph.innerHTML = `A validação retornou ${result}`;
+};
+
+const isUrl = () => {
+  const result = validator.isURL(input.value);
+  paragraph.innerHTML = `A validação retornou ${result}`;
+};
+
+const isLower = () => {
+  const result = validator.isLowercase(input.value);
+  paragraph.innerHTML = `A validação retornou ${result}`;
+};
+
+const isHexColor = () => {
+  const result = validator.isHexColor(input.value);
+  paragraph.innerHTML = `A validação retornou ${result}`;
+};
+
+validateBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (select.value === 'Email') {
+    isEmail();
+  }
+
+  if (select.value === 'CPF') {
+    isTaxId();
+  }
+
+  if (select.value === 'Url') {
+    isUrl();
+  }
+
+  if (select.value === 'Lower') {
+    isLower();
+  }
+
+  if (select.value === 'Hex') {
+    isHexColor();
+  }
+});
