07d24a4ee1e3b19388ca851f13bf760e879c234d
Fabio Lessa <contatofabiolessa@gmail.com>
2024-03-04 15:49:56 -0300

    Aula 3

--- back-end/secao-05-orm-e-autenticacao/dia-03-relacionamento-n-n/books-api-sequelize/src/config/config.js
+++ back-end/secao-05-orm-e-autenticacao/dia-03-relacionamento-n-n/books-api-sequelize/src/config/config.js
@@ -3,6 +3,7 @@ const config = {
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   host: process.env.MYSQL_HOST,
+  port: process.env.PORT,
   dialect: 'mysql',
 };
 
